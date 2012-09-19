9aa6862d32e7aedd1c7402b29e3abdd1d716d26a
Oivvio Polite <oivvio@liberationtech.net>
2012-09-19 12:08:31 +0400

    basics, added our first model

---
+++ databasecreation.sh
@@ -0,0 +1,13 @@
+db=$1
+pw=$2
+user=$db
+echo $db $user $pw
+tmpfile=$(mktemp)
+
+echo "" > $tmpfile
+#echo "CREATE DATABASE $db CHARACTER SET utf8;" >> $tmpfile
+#echo "CREATE USER '$user'@'localhost' IDENTIFIED BY '$pw';" >> $tmpfile
+echo "GRANT ALL PRIVILEGES ON $db.* TO '$user'@'localhost' WITH GRANT OPTION;" >> $tmpfile
+
+mysql < $tmpfile
+rm -f $tmpfile
