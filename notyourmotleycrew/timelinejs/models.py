041378eabe28ac3b6f52d0fc23fd6d2f387f6d04
Oivvio Polite <oivvio@liberationtech.net>
2012-09-23 15:19:16 +0400

    added the tweet button

--- notyourmotleycrew/timelinejs/models.py
+++ notyourmotleycrew/timelinejs/models.py
@@ -7,20 +7,28 @@ class Timeline(models.Model):
     type = models.CharField(max_length = 20, default="default")
     startdate =  models.DateTimeField()
     text = models.TextField()
-    media = models.URLField()
-    blockquote = models.TextField()
-    credit = models.CharField(max_length = 400) 
-    caption = models.CharField(max_length = 400)
+    media = models.URLField(blank=True)
+    blockquote = models.TextField(blank=True)
+    credit = models.CharField(max_length = 400, blank=True) 
+    caption = models.CharField(max_length = 400, blank=True)
+
+    def get_json(self):
+        for event in self.event.all():
+            print event
 
 class Event(models.Model):
     
     headline = models.CharField(max_length = 300)
     startdate =  models.DateTimeField()
-    text = models.TextField()
-    media = models.URLField()
-    blockquote = models.TextField()
-    credit = models.CharField(max_length = 400) 
-    caption = models.CharField(max_length = 400)
+    text = models.TextField(blank=True)
+    media = models.URLField(blank=True)
+    blockquote = models.TextField(blank=True)
+    credit = models.CharField(max_length = 400, blank=True) 
+    caption = models.CharField(max_length = 400, blank=True)
 
-    timeline = models.ForeignKey(Timeline)
+    timeline = models.ForeignKey(Timeline, related_name="event")
+
+    def get_json(self):
+        pass
+        #TODO
 
